c9f22cec56a2774a5fc922a179ee10698197da8e
chiling <guanguanjujiu@qq.com>
2018-12-22 11:58:19 +0800

    add

--- matrixmodel.cpp
+++ matrixmodel.cpp
@@ -1,15 +1,36 @@
-#ifndef M_NO_DEBUG
-#include <QDebug>
-#endif
-
 #include "matrixmodel.h"
 
-MatrixModel::MatrixModel(MatrixSize x, MatrixSize y)
+MatrixModel::MatrixModel(MatrixSize size, ModelPattern pattern):
+    currentModel(size*size, 0),
+    modelPattern(pattern),
+    modelSize(size),
+    updateStatus(false),
+    updateLine(0)
 {
-    currentModelP = new vector<int>(x*y, 0);
 }
 
 MatrixModel::~MatrixModel()
 {
-    currentModelP->clear();
+    currentModel.clear();
+}
+
+void MatrixModel::clearUnit(MatrixSize x, MatrixSize y, MatrixSize widht, MatrixSize height)
+{
+#ifndef M_NO_DEBUG
+    if(x > modelSize || y > modelSize)
+        qDebug() << "log in " << __FILE__ << ":" << __FUNCTION__ << " line: " << __LINE__
+                 << "error: Index out of range!";
+#endif
+    for(MatrixSize i = 0; i < widht; ++i)
+    {
+        for(MatrixSize j = 0; j < height; ++j)
+        {
+            currentModel[x+i + (y+j)*modelSize] = 0;
+        }
+    }
+}
+
+void MatrixModel::clearAllUnit()
+{
+    clearUnit(0, 0, modelSize, modelSize);
 }
--- matrixmodel.h
+++ matrixmodel.h
@@ -5,27 +5,56 @@
 #include <QMutex>
 #include <vector>
 
+#ifndef M_NO_DEBUG
+#include <QDebug>
+#endif
+
 using MatrixSize = std::vector<int>::size_type;
 using std::vector;
 
 class MatrixModel
 {
 public:
-    MatrixModel(MatrixSize x, MatrixSize y);
+    enum ModelPattern{ RedKernel, LifeGame };
+
+    MatrixModel(MatrixSize widht, ModelPattern pattern);
     ~MatrixModel();
 
-    int getUnitValue(MatrixSize x, MatrixSize y);		//获取单元值
-    void updateModel();									//更新数据
-    void changeModelValue(MatrixSize x, MatrixSize y);	//修改单元的值
-    void clearModel(MatrixSize x, MatrixSize y, MatrixSize widht, MatrixSize height);	//清空单元值
+    int getUnitValue(MatrixSize x, MatrixSize y) const;		//获取单元值
+    void (*updateModel)();									//更新数据
+    void changeModelValue(MatrixSize x, MatrixSize y, int value);	//修改单元的值
+    void clearUnit(MatrixSize x, MatrixSize y, MatrixSize widht, MatrixSize height);	//清空单元值
+    void clearAllUnit();
     
 private:
-    vector<int> *currentModelP;
+    vector<int> currentModel;
+    ModelPattern modelPattern;
+    MatrixSize modelSize;
     
     bool updateStatus;
     MatrixSize updateLine;
     QMutex lineMutex;
     QMutex changeMutex;
 };
+
+inline int MatrixModel::getUnitValue(MatrixSize x, MatrixSize y) const
+{
+#ifndef M_NO_DEBUG
+    if(x > modelSize || y > modelSize)
+        qDebug() << "log in " << __FILE__ << ":" << __FUNCTION__ << " line: " << __LINE__
+                 << "error: Index out of range!";
+#endif
+    return currentModel[x+y*modelSize];
+}
+
+inline void MatrixModel::changeModelValue(MatrixSize x, MatrixSize y, int value)
+{
+#ifndef M_NO_DEBUG
+    if(x > modelSize || y > modelSize)
+        qDebug() << "log in " << __FILE__ << ":" << __FUNCTION__ << " line: " << __LINE__
+                 << "error: Index out of range!";
+#endif
+    currentModel[x+y*modelSize] = value;
+}
 
 #endif // MATRIXMODEL_H
